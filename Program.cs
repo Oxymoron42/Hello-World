fb7c8128cd967593ff8edc8c8541bc531ec4bc81
Oxymoron42 <grigoryev.uk@gmail.com>
2017-02-25 15:04:20 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
